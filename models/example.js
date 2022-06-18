a3b41200db00773e06730c0f8c738d23afac47f7
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-06-18 11:38:23 -0500

    renamed controllers dir to config

--- models/example.js
+++ models/example.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 class Example extends Model {};
 
-const sequelize = require('../controllers/connection.js');
+const sequelize = require('../config/connection.js');
 
 Example.init(
     {
