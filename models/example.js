24992824a5f63b0f674586166c94e7b3707831a8
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-06-18 11:13:50 -0500

    created example model

---
+++ models/example.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Example extends Model {};
+
+const sequelize = require('../controllers/connection.js');
+
+Example.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2, 32]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+      }
+)
+
