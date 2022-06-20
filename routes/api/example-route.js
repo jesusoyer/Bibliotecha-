67111bb3c4af0debe5fc9d6e53bb844e08ac520c
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-06-20 13:54:27 -0500

    added get route for id

--- routes/api/example-route.js
+++ routes/api/example-route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const  Example  = require('../../models/example.js');
 
+//get all data records
 router.get('/', async (req, res) => {
     try {
       const exampleData = await Example.findAll({});
@@ -11,5 +12,20 @@ router.get('/', async (req, res) => {
       res.status(500).json(err)
     }
     });
+
+//get one data record by its id value
+router.get('/:id', async (req, res) => {
+  try {
+    const exampleData = await Example.findByPk(req.params.id, {});
+    if (!exampleData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(exampleData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+  });
 
     module.exports = router;
