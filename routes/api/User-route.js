d5ebf96d1274b0aa11135b2114a14aacfd3b1e8c
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-06-22 19:02:53 -0500

    created post routes to create/login a user

--- routes/api/User-route.js
+++ routes/api/User-route.js
@@ -1,2 +1,60 @@
 const router = require('express').Router();
 const  User  = require('../../models/example.js');
+
+// create new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+});
+
+// login
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (!userData) {
+            res
+            .status(400)
+            .json({ message: 'User not found!'});
+            return;
+        }
+
+        const correctPassword = await userData.checkPassword(req.body.password);
+
+        if (!correctPassword) {
+            res
+            .status(400)
+            .json({ massage: 'Incorrect email or password! Please try again.'})
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res
+            .status(200)
+            .json({ message: 'You are now logged in!'});
+        });
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err);
+    }
+});
