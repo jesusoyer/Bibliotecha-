104b6880895e18d6af411ff0f2f80b8b875d9d7b
jesusoyer <oyervidesjesus017@gmail.com>
2022-06-22 20:32:22 -0500

    created login page and connected its route

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -13,8 +13,6 @@ router.get('/', async (req, res) => {
     );
    
        
-        
-        
 
     res.render('homepage', {
       exam, 
@@ -25,5 +23,16 @@ router.get('/', async (req, res) => {
       res.status(500).json(err)
     }
 });
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //   res.redirect('/profile');
+    //   return;
+    // }
+  
+    res.render('login');
+  });
+
 
 module.exports = router;
