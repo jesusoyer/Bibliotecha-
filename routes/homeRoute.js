f006623f05f0ec9016d978aa4f0e9017c2b70f78
jesusoyer <oyervidesjesus017@gmail.com>
2022-06-25 11:58:30 -0500

    changes

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,63 +1,28 @@
+const Example = require('../models/example')
 const router = require('express').Router();
-// const User = require('../models/User')
-const Example = require('../models/example');
 
 router.get('/', async (req, res) => {
-  try {
-    const exampleData = await Example.findAll({});
-    //console.log(exampleData)
-    // res.status(200).json(exampleData);
-  
-    const exam = exampleData.map((exampleData) =>
-    exampleData.get({ plain: true })
-  );
- 
-     
-
-  res.render('homepage', {
-    exam, 
+    try {
+      const exampleData = await Example.findAll({});
+      console.log(exampleData)
+      // res.status(200).json(exampleData);
     
-  });   
-  } catch (err) {
-    console.log(err)
-    res.status(500).json(err)
-  }
-});
-
-
-
-
-// router.get('/', async (req, res) => {
-//     try {
-//       const userData = await User.findAll({});
-//       console.log(userData)
-//       // res.status(200).json(userData);
-    
-//       const exam = userData.map((userData) =>
-//       userData.get({ plain: true })
-//     );
+      const exam = exampleData.map((exampleData) =>
+      exampleData.get({ plain: true })
+    );
    
        
+        
+        
 
-//     res.render('homepage', {
-//       exam, 
+    res.render('homepage', {
+      exam, 
       
-//     });   
-//     } catch (err) {
-//       console.log(err)
-//       res.status(500).json(err)
-//     }
-// });
-
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    // if (req.session.logged_in) {
-    //   res.redirect('/profile');
-    //   return;
-    // }
-  
-    res.render('login');
-  });
-
+    });   
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(err)
+    }
+});
 
 module.exports = router;
