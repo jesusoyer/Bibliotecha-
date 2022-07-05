86aa9824c08f7345b32c3a1780855614ce11dd90
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-07-05 14:56:32 -0500

    fixed delete button

--- public/js/delete.js
+++ public/js/delete.js
@@ -2,14 +2,16 @@ const deletePost = async (event) => {
     event.preventDefault();
     const isbnId = document.querySelector('#isbnValue'); 
   console.log("delete route hit")
-//   const isbn = isbnId.textContent;
+  const isbn = isbnId.textContent;
   console.log(isbn)
-      const response = await fetch(`/api/books/${isbnId}`, {
+      const response = await fetch(`/api/books/${isbn}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        console.log("YOU ARE HERE")
+        console.log(response)
+        document.location.replace('/');
       } else {
         alert('Failed to delete project');
       }
